03f4410bd7a192b9e0ca8850c6d6c09636c4ac1b
AsuraHu <1030320304@qq.com>
2019-11-11 17:27:37 +0800

    vue

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <el-row :gutter="20">
+      <el-col :span="6"><div class="grid-content bg-purple">1</div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple-dark">2</div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple">3</div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple">4</div></el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+  .el-row {
+    margin-bottom: 0px;
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #0f0;
+  }
+  .bg-purple {
+    background: #f00;
+  }
+
+  .grid-content {
+    text-align: center;
+    line-height: 36px;
+    border-radius: 4px;
+    min-height: 36px;
+  }
+
+</style>
---
+++ src/pages/list/List.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>ldgksgl;s</div>
+</template>
+
+<script>
+export default {
+  name: 'List'
+}
+</script>
+
+<style scoped>
+
+</style>
